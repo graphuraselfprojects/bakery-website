9c629a38bad99c4cb3fa01bf4f236fdc42e1a47f
vivekkr10 <vivekkr48027@gmail.com>
2026-01-06 15:00:05 +0530

    corrected vercel link

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,8 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://bakery-website-inky.vercel.app",
-      "bakery-website-git-main-graphuraselfprojects-projects.vercel.app",
-      "bakery-website-n98z3ufn8-graphuraselfprojects-projects.vercel.app",
-      // "https://bakery-oa9j-vboodczv7-graphura-india-pvt-ltds-projects.vercel.app", 
+      "https://bakery-website-git-main-graphuraselfprojects-projects.vercel.app",
+      "https://bakery-website-n98z3ufn8-graphuraselfprojects-projects.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
